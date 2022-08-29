985d7f87dfee09587afbc51c7bb2b678de2b736c
NikolaFloki <ilicalokin@gmail.com>
2022-08-29 16:50:36 +0200

    all

---
+++ studybud/base/views.py
@@ -0,0 +1,37 @@
+from django.shortcuts import render, redirect
+from .models import Room
+from .forms import RoomForm
+
+
+def home(request):
+    rooms = Room.objects.all()
+    context = {'rooms': rooms}
+    return render(request,'base/home.html', context)
+
+
+def room(request, id):
+    room = Room.objects.get(id=id)
+    context = {'room':  room} 
+
+    return render(request, 'base/room.html',  context)
+
+
+def createRoom(request):
+    form = RoomForm()
+    if request.method == 'POST':
+        form = RoomForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, id):
+    room = Room.objects.get(id=id)
+
+    context = {}
+    return render(request, 'base/room_form.html', context)
+
+
